549b72476e49080310b701c0a34c265c7d924a9b
S-Taichiii <ab20082@shibaura-it.ac.jp>
2024-12-07 01:13:56 +0900

    create option

--- src/main.js
+++ src/main.js
@@ -2,40 +2,63 @@ const config = {
   url: "https://api.recursionist.io/builder/computers?type=",
 };
 
-async function fetchData(parts) {
-  let endpoint = config.url + parts;
+function initializeSelect(id) {
+  let select = document.getElementById(id);
+  select.innerHTML = "";
 
-  return fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "-";
+  defaultOption.textContent = "-";
+  select.appendChild(defaultOption);
 }
 
-function createOption(data, id, optionName) {
-  let selector = document.getElementById(id);
+function createOption(partsName) {
+  let brandId = partsName + "Brand";
+  let modelId = partsName + "Model";
+  let brand = document.getElementById(brandId);
+  let model = document.getElementById(modelId);
+  let endpoint = config.url + partsName;
 
-  if (optionName == "Brand") {
-    data.forEach((item) => {
-      const option = document.createElement("option");
-      option.value = item.Brand;
-      option.textContent = item.Brand;
-      selector.appendChild(option);
-    });
-  } else if (optionName == "Model") {
-    data.forEach((item) => {
-      const option = document.createElement("option");
-      option.value = item.Model;
-      option.textContent = item.Model;
-      selector.appendChild(option);
-    });
-  }
-}
+  brand.addEventListener("focus", () => {
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((item) => {
+          // brand名がoptionにあるか確認、なければoptionを追加
+          let isBrand = false;
+          for (let i = 0; i < brand.options.length; i++) {
+            if (brand.options[i].value === item.Brand) {
+              isBrand = true;
+              break;
+            }
+          }
 
-fetchData("cpu")
-  .then((data) => {
-    createOption(data, "cpuBrand", "Brand");
-  })
-  .catch((error) => {
-    console.log(error);
+          if (!isBrand) {
+            const option = document.createElement("option");
+
+            option.value = item.Brand;
+            option.textContent = item.Brand;
+            brand.appendChild(option);
+          }
+        });
+
+        brand.addEventListener("change", (e) => {
+          initializeSelect(modelId);
+
+          data.forEach((item) => {
+            if (e.target.value === item.Brand) {
+              const option = document.createElement("option");
+
+              option.value = item.Model;
+              option.textContent = item.Model;
+              model.appendChild(option);
+            }
+          });
+        });
+      });
   });
+}
+
+createOption("cpu");
+createOption("gpu");
+createOption("ram");
