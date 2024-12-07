d50711baf066cc065c6ffdff59ef3daadf4e3be2
S-Taichiii <ab20082@shibaura-it.ac.jp>
2024-12-07 12:46:26 +0900

    create memoryOption()

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const config = {
   url: "https://api.recursionist.io/builder/computers?type=",
 };
 
+// selectの中を初期化
 function initializeSelect(id) {
   let select = document.getElementById(id);
   select.innerHTML = "";
@@ -12,12 +13,12 @@ function initializeSelect(id) {
   select.appendChild(defaultOption);
 }
 
-function createOption(partsName) {
+function createCpuAndGpuOption(partsName) {
+  let endpoint = config.url + partsName;
   let brandId = partsName + "Brand";
   let modelId = partsName + "Model";
   let brand = document.getElementById(brandId);
   let model = document.getElementById(modelId);
-  let endpoint = config.url + partsName;
 
   brand.addEventListener("focus", () => {
     fetch(endpoint)
@@ -42,6 +43,7 @@ function createOption(partsName) {
           }
         });
 
+        // Brandのvalueによってmodelのオプションを変更
         brand.addEventListener("change", (e) => {
           initializeSelect(modelId);
 
@@ -59,6 +61,72 @@ function createOption(partsName) {
   });
 }
 
-createOption("cpu");
-createOption("gpu");
-createOption("ram");
+function validateMemory(modelName, value) {
+  let modelString = modelName.split(" ");
+  let modelSize = modelString[modelString.length - 1];
+
+  console.log(modelSize[0] === value);
+  return modelSize[0] === value;
+}
+
+function createMemoryOption() {
+  let endpoint = config.url + "ram";
+  let brandId = "ramBrand";
+  let modelId = "ramModel";
+  let number = document.getElementById("howMany");
+  let brand = document.getElementById(brandId);
+  let model = document.getElementById(modelId);
+
+  number.addEventListener("change", (numberEvent) => {
+    initializeSelect(brandId);
+    initializeSelect(modelId);
+
+    brand.addEventListener("focus", () => {
+      fetch(endpoint)
+        .then((response) => response.json())
+        .then((data) => {
+          data.forEach((item) => {
+            // brand名がoptionにあるか確認、なければoptionを追加
+            let isBrand = false;
+            for (let i = 0; i < brand.options.length; i++) {
+              if (brand.options[i].value === item.Brand) {
+                isBrand = true;
+                break;
+              }
+            }
+
+            if (!isBrand) {
+              const option = document.createElement("option");
+
+              option.value = item.Brand;
+              option.textContent = item.Brand;
+              brand.appendChild(option);
+            }
+          });
+
+          // Brandのvalueによってmodelのオプションを変更
+          brand.addEventListener("change", (e) => {
+            initializeSelect(modelId);
+
+            data.forEach((item) => {
+              if (
+                e.target.value === item.Brand &&
+                validateMemory(item.Model, numberEvent.target.value)
+              ) {
+                const option = document.createElement("option");
+
+                option.value = item.Model;
+                option.textContent = item.Model;
+                model.appendChild(option);
+              }
+            });
+          });
+        });
+    });
+  });
+}
+
+createCpuAndGpuOption("cpu");
+createCpuAndGpuOption("gpu");
+
+createMemoryOption();
